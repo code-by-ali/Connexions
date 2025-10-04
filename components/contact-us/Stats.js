4525d077238ed2b8a050950feeb26edd2e1075cd
Mohammed Ali <codebyali17@gmail.com>
2025-10-04 13:36:14 +0530

    small fix

--- components/contact-us/Stats.js
+++ components/contact-us/Stats.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Stats = () => {
   const commonClassWrapper =
-    "flex flex-col md:flex-row items-center md:items-start gap-2 md:gap-4";
+    "flex flex-col md:flex-row items-center md:items-start gap-2 md:gap-4 h-full";
 
   const commonClassHeader =
     "text-primary text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold";
@@ -10,7 +10,7 @@ const Stats = () => {
     "text-[#4E5B6D] text-sm sm:text-base md:text-lg text-center md:text-left";
 
   return (
-    <div className="w-full px-6 md:px-20 py-10 flex flex-col md:flex-row justify-between items-center gap-8">
+    <div className="w-full px-6 md:px-20 py-10 flex flex-col md:flex-row justify-between items-stretch gap-8">
       <div className={commonClassWrapper}>
         <span className={commonClassHeader}>10K+</span>
         <span className={commonClassSubtitle}>
@@ -20,7 +20,7 @@ const Stats = () => {
       <div className={commonClassWrapper}>
         <span className={commonClassHeader}>95%</span>
         <span className={commonClassSubtitle}>
-          Client retention rate across<br />  industries.
+          Client retention rate across<br /> industries.
         </span>
       </div>
       <div className={commonClassWrapper}>
